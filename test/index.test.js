4a2435597b8e8882c6ed5c5dfab5eac62298d323
BekirLee <nihadbekirli8@gmail.com>
2024-10-29 01:47:03 +0400

    just some changes and updates on codes but having error on tests

--- test/index.test.js
+++ test/index.test.js
@@ -1,12 +1,71 @@
-import chaiHttp from "chai-http";
-import chai from 'chai'
-import server from "../app.js";
-const should = chaiDependencie.should();
+// import * as chai from 'chai';
+// import * as chaiHttp from 'chai-http';
+// import server from '../app.js';
 
+// chai.use(chaiHttp);
+// // const { expect } = chai;
+
+// describe('Node server tests', () => {
+//     it("(GET/ ) for the main page!", (done) => {
+//         chai.request(server)
+//             .get('/')
+//             .end((err, res) => {
+//                 res.should.have.status(200);
+//                 done();
+//             });
+//     });
+// });
+
+import * as chai from 'chai';
+import chaiHttp from 'chai-http';
+import { request } from 'chai-http';
+import server from '../app.js';
+
+const { expect } = chai;
 chai.use(chaiHttp);
 
 describe('Node server tests', () => {
-    it("(GET/ ) for the main page! ", (done) => {
-        done()
-    })
-})
+  it("GET / - should return the main page", done => {
+    request(server)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
+
+
+
+// import * as chai from 'chai';
+// import * as chaiHttp from 'chai-http';
+// chai.use(chaiHttp)
+
+// describe('Chat', () => {
+//     it('should return all data',async()=>{
+//                 request("https://google.com")
+//                 .get('/')
+//                 .end((err, res) => {
+//                  expect(res).to.have.status(200);
+//                  done();
+//                  });
+//             })
+//      })
+
+// import * as chai from 'chai';
+// import * as chaiHttp from 'chai-http';
+// import server from "../app.js";
+
+// chai.use(chaiHttp);
+// const should = chai.should();
+
+// describe('Node server tests', () => {
+//     it("(GET/ ) for the main page!", (done) => {
+//         chai.request(server)
+//             .get('/')
+//             .end((err, res) => {
+//                 res.should.have.status(200); // Durum kodu 200'ü kontrol et
+//                 done();
+//             });
+//     });
+// });
